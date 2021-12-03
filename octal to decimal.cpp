26993ea340f235f968324da5624f8e8895bd9baf
Dr-CATastrophe <95492728+Dr-CATastrophe@users.noreply.github.com>
2021-12-03 21:51:56 +0500

    Update octal to decimal.cpp

--- octal to decimal.cpp
+++ octal to decimal.cpp
@@ -5,10 +5,10 @@ int oct_dec(int oct)
 {
 	int deci=0;
 	int rem=1;
-	for(int i=0;oct!=0;i++)//or i<=len
+	for(int i=0;oct!=0;i++)
 	{
-		rem=oct%10;
-		oct=oct/10;
+		rem=oct%10;//get the last digit of the number stored in 'oct'
+		oct=oct/10;//update 'oct' to remove its last digit
 		deci=deci+rem*pow(8,i);
 	}
 	return deci;
@@ -19,8 +19,8 @@ int main()
 	int oct;
 	printf("Enter a number in octal(0-7) to convert to decimal(0-9):");
 	scanf("%d",&oct);
-	oct_dec(oct);
-	printf("The octal number %d converted to decimal is: %d",oct,oct_dec(oct));
+	int result=oct_dec(oct);
+	printf("The octal number %d converted to decimal is: %d",oct,result);
 	
 }
 
